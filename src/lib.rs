36b76162e7e01dc5c86ae546418499ccd37efb00
Tom Webber <t.c.w.webber@gmail.com>
2025-03-16 22:38:26 +0100

    impl: line history

--- src/lib.rs
+++ src/lib.rs
@@ -4,55 +4,86 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode},
 };
 
-type LineContent = String;
-
-pub struct App<F: Fn(LineContent)> {
-    line_func: F,
-    current_line: String,
-    line_cursor: usize,
+#[derive(Default)]
+struct Line {
+    content: String,
+    cursor: usize,
 }
 
-impl<F: Fn(LineContent)> From<F> for App<F> {
-    fn from(line_func: F) -> Self {
-        enable_raw_mode().expect("Terminal not supported. Please raise an issue.");
-
-        Self {
-            line_func,
-            current_line: String::new(),
-            line_cursor: 0,
-        }
-    }
-}
-
-impl<F: Fn(LineContent)> App<F> {
+impl Line {
     fn insert(&mut self, ch: char) {
-        self.current_line.insert(self.line_cursor, ch);
+        self.content.insert(self.cursor, ch);
     }
     fn backspace(&mut self) {
-        if self.line_cursor > 0 {
-            self.current_line.pop();
-            self.line_cursor -= 1;
+        if self.cursor > 0 {
+            self.content.pop();
+            self.cursor -= 1;
         }
     }
     fn decrease_counter(&mut self) {
-        if self.line_cursor > 0 {
-            self.line_cursor -= 1;
+        if self.cursor > 0 {
+            self.cursor -= 1;
         }
     }
+    fn increase_counter(&mut self) {
+        self.cursor += 1;
+    }
+    fn take(&mut self) -> String {
+        take(self).content
+    }
+}
 
+#[derive(Default)]
+struct History {
+    content: Vec<Box<str>>,
+    cursor: usize,
+}
+
+impl History {
+    fn push(&mut self, line: Box<str>) {
+        self.content.push(line);
+    }
+}
+
+pub struct App<F: Fn(&str)> {
+    action: F,
+    line: Line,
+    history: History,
+}
+
+impl<F: Fn(&str)> From<F> for App<F> {
+    fn from(action: F) -> Self {
+        enable_raw_mode().expect("Terminal not supported. Please raise an issue.");
+
+        Self {
+            action,
+            line: Line::default(),
+            history: History::default(),
+        }
+    }
+}
+
+impl<F: Fn(&str)> App<F> {
     pub fn run(&mut self) {
         while self.step() {}
         disable_raw_mode().expect("Terminal not supported. Please raise an issue.");
     }
 
+    fn take_action(&mut self) {
+        let line = self.line.take();
+        (self.action)(&line);
+        self.history.push(line.into_boxed_str());
+    }
+
     fn step(&mut self) -> bool {
         if let Event::Key(key) = read().unwrap() {
             match key.code {
-                KeyCode::Enter => (self.line_func)(take(&mut self.current_line)),
-                KeyCode::Char(ch) => self.insert(ch),
-                KeyCode::Backspace => self.backspace(),
+                KeyCode::Enter => self.take_action(),
+                KeyCode::Char(ch) => self.line.insert(ch),
+                KeyCode::Backspace => self.line.backspace(),
                 KeyCode::Esc => return false,
-                KeyCode::Left => self.decrease_counter(),
+                KeyCode::Left => self.line.decrease_counter(),
+                KeyCode::Right => self.line.increase_counter(),
                 _ => (),
             }
         }
